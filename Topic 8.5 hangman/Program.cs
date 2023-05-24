92d27c7c82c3247c4b4fb3cfbddc37542b564d25
Jack Eitel <339481210@GoTVDSB.ca>
2023-05-24 09:51:22 -0400

    done

--- Topic 8.5 hangman/Program.cs
+++ Topic 8.5 hangman/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Topic_8._5_hangman
+﻿using System;
+
+namespace Topic_8._5_hangman
 {
     internal class Program
     {
@@ -6,8 +8,10 @@
         {
             Random generator = new Random();    
             List<string> words = new List<string>();
-            string word;
+            string word, word2 = "";
             string guess;
+            int lives = 6;
+            int lettersGuessed = 0;
             words.Add("ugliest");
             words.Add("piquant");
             words.Add("front");
@@ -28,8 +32,35 @@
             words.Add("solve");
             words.Add("gate");
             words.Add("apples");
+            words.Add("harm");
+            words.Add("chalk");
+            words.Add("issue");
+            words.Add("routine");
+            words.Add("Bible");
+            words.Add("surround");
+            words.Add("offer");
+            words.Add("choose");
+            words.Add("conceive");
+            words.Add("remunerate");
+            words.Add("coffee");
+            words.Add("lip");
+            words.Add("snail");
+            words.Add("gift");
+            words.Add("dialogue");
+            words.Add("metal");
+            words.Add("greeting");
+            words.Add("dismiss");
+            words.Add("eaux");
+            words.Add("frog");
 
-            word = words[generator.Next(0, 21)];
+
+
+            word = words[generator.Next(words.Count)];
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                word2 += "_ ";
+            }
             Console.WriteLine("=======|");
             Console.WriteLine("   |   |");
             Console.WriteLine("       |");
@@ -39,12 +70,129 @@
             Console.WriteLine("       |");
             Console.WriteLine("===========");
             Console.WriteLine();
-            Console.WriteLine("Guess a letter");
-            guess = Console.ReadLine().Trim();
-            while (guess.Length != 1)
+            while (lives > 0 || lettersGuessed == word.Length)
             {
+                // Console.WriteLine(word);
+                Console.WriteLine();
+                Console.WriteLine("Guess a letter");
+                guess = Console.ReadLine().Trim();
+                while (guess.Length != 1)
+                {
+                    if (guess.Length != 1)
+                    {
+                        Console.WriteLine("Guess is more than one character");
+                    }
+                    guess = Console.ReadLine().Trim().ToLower();
+                }
 
+                if (word.Contains(guess))
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == guess[0])
+                        {
+                            word2 = word2.Remove(i * 2,1);
+                            word2 = word2.Insert(i * 2, Convert.ToString(guess[0]));
+                            lettersGuessed++;
+
+                        }
+
+                    }
+                }
+                else if (word.IndexOf(guess) == -1)
+                {
+                    lives--;
+
+                }
+                if (lettersGuessed == word.Length)
+                {
+                    Console.WriteLine("Congrats, you win");
+                }
+                Console.WriteLine();
+                Console.WriteLine(word2);
+                switch (lives)
+                {
+                    case 6:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("   |\\  |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("  /|\\  |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 1:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("  /|\\  |");
+                        Console.WriteLine("    \\  |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        break;
+                    case 0:
+                        Console.WriteLine("=======|");
+                        Console.WriteLine("   |   |");
+                        Console.WriteLine("   O   |");
+                        Console.WriteLine("  /|\\  |");
+                        Console.WriteLine("  / \\  |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("       |");
+                        Console.WriteLine("===========");
+                        Console.WriteLine();
+                        Console.WriteLine("The word was " + word);
+                        break;
+                }
             }
+            
             
         }
     }
